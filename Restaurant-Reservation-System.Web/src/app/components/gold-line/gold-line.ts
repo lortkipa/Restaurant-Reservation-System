bfcc07a97fad13cbe129fcb0a17e6dbd494fec68
lortkipa <nikusha191208@gmail.com>
2026-03-24 22:16:04 +0400

    added gold-line component

---
+++ Restaurant-Reservation-System.Web/src/app/components/gold-line/gold-line.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-gold-line',
+  imports: [],
+  templateUrl: './gold-line.html',
+  styleUrl: './gold-line.scss',
+})
+export class GoldLine {}
--- Restaurant-Reservation-System.Web/src/app/components/home/home.ts
+++ Restaurant-Reservation-System.Web/src/app/components/home/home.ts
@@ -2,11 +2,12 @@ import { Component, ElementRef, ViewChild } from '@angular/core';
 import { Hero } from "../hero/hero";
 import { Restaurants } from "../restaurants/restaurants";
 import { ReservationForm } from "../reservation-form/reservation-form";
+import { GoldLine } from "../gold-line/gold-line";
 
 @Component({
   standalone: true,
   selector: 'app-home',
-  imports: [Hero, Restaurants, ReservationForm],
+  imports: [Hero, Restaurants, ReservationForm, GoldLine],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
