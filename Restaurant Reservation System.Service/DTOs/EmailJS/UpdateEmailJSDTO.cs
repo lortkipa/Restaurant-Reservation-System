096e9daea9b6e6e7b16cd38d42ec57eb8da30be4
lortkipa <nikusha191208@gmail.com>
2026-04-06 14:22:06 +0400

    removed UserId from UpdateEmailJSDTO

--- Restaurant Reservation System.Service/DTOs/EmailJS/UpdateEmailJSDTO.cs
+++ Restaurant Reservation System.Service/DTOs/EmailJS/UpdateEmailJSDTO.cs
@@ -6,7 +6,7 @@ namespace Restaurant_Reservation_System.Service.DTOs.EmailJS
 {
     public class UpdateEmailJSDTO
     {
-        public int UserId { get; set; }
+        //public int UserId { get; set; }
         public string? ServiceId { get; set; }
         public string? TemplateId { get; set; }
         public string? PublicKey { get; set; }
